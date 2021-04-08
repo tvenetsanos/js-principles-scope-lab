1db96dfc0766ff72607aeb2ae211377e395206f9
Tyler <tyler_venetsanos@msn.com>
2021-04-08 12:36:09 -0400

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Write your solution in this file!
+customerName = "bob"
+
+function upperCaseCustomerName(){
+    customerName = customerName.toUpperCase()
+}
+
+function setBestCustomer(){
+    bestCustomer = "not bob"
+    return bestCustomer
+}
+
+function overwriteBestCustomer(){
+    bestCustomer = "maybe bob"
+    return bestCustomer
+}
+
+const leastFavoriteCustomer = "karen"
+
+function changeLeastFavoriteCustomer(){
+    return leastFavoriteCustomer = "jill"
+}
